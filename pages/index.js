eab6158e71adfc28143b052404876c9ee67fad8d
RafaelMagalhaes-const <rafaelgabriel20@hotmail.com>
2026-04-05 21:19:46 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Parabens meu amor, você é uma mulher incrivel, estou orgulhoso de você </h1>;
+  return <h1>Parabens meu amor, você é uma mulher incrivel, estou orgulhoso de você</h1>;
 }
 
 export default Home;
